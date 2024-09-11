e90ac5d02f43e62e4e1937c51e660ddb2068c2bf
Satrio Kamaludin <satriokamaludin@gmail.com>
2024-09-11 10:42:55 +0700

    api export fix

--- fe-nextjs/src/app/api/api.js
+++ fe-nextjs/src/app/api/api.js
@@ -81,7 +81,7 @@ export const downloadFile = async () => {
     const url = window.URL.createObjectURL(new Blob([response.data]));
     const link = document.createElement('a');
     link.href = url;
-    link.download = 'jobs-export.xlsx'; // or the appropriate file name
+    link.download = 'jobs-export.xls'; // or the appropriate file name
     document.body.appendChild(link);
     link.click();
     link.remove();
